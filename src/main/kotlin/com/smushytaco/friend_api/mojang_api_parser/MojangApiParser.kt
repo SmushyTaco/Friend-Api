551a90beba5d0c1909a09ce4e4fbc043bad7de34
Nikan Radan <github.com@alias.nikanradan.com>
2025-07-18 00:26:34 -0700

    1.21.8

--- src/main/kotlin/com/smushytaco/friend_api/mojang_api_parser/MojangApiParser.kt
+++ src/main/kotlin/com/smushytaco/friend_api/mojang_api_parser/MojangApiParser.kt
@@ -1,36 +1,17 @@
 package com.smushytaco.friend_api.mojang_api_parser
 import kotlinx.serialization.json.Json
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.util.*
 object MojangApiParser {
     private val client = OkHttpClient()
     private val json = Json { ignoreUnknownKeys = true }
-    private fun executeRequestForGets(request: Request): NameAndUUID? {
-        return try {
-            client.newCall(request).execute().use { response -> response.body?.string()?.let { json.decodeFromString(NameAndUUID.serializer(), it) } }
-        } catch (_: Exception) { null }
-    }
-    private fun executeRequestForChecks(request: Request): Boolean {
-        return try {
-            client.newCall(request).execute().use { response -> response.isSuccessful }
-        } catch (_: Exception) { false }
-    }
-    fun getUuid(name: String): NameAndUUID? {
-        val request = Request.Builder().url("https://api.mojang.com/users/profiles/minecraft/$name/").build()
-        return executeRequestForGets(request)
-    }
-    fun getUsername(uuid: UUID): NameAndUUID? {
-        val request = Request.Builder().url("https://sessionserver.mojang.com/session/minecraft/profile/${uuid.toString().replace("-", "")}/").build()
-        return executeRequestForGets(request)
-    }
+    private fun executeRequestForGets(request: Request) = runCatching { client.newCall(request).execute().use { response -> json.decodeFromString(NameAndUUID.serializer(), response.body.string()) } }.getOrNull()
+    private fun executeRequestForChecks(request: Request) = runCatching { client.newCall(request).execute().use { response -> response.isSuccessful } }.getOrElse { false }
+    fun getUuid(name: String) = executeRequestForGets(Request(url = "https://api.mojang.com/users/profiles/minecraft/$name/".toHttpUrl()))
+    fun getUsername(uuid: UUID) = executeRequestForGets(Request(url = "https://sessionserver.mojang.com/session/minecraft/profile/${uuid.toString().replace("-", "")}/".toHttpUrl()))
     @Suppress("UNUSED")
-    fun isUsernameToUuidApiRunning(): Boolean {
-        val request = Request.Builder().url("https://api.mojang.com/users/profiles/minecraft/SmushyTaco/").build()
-        return executeRequestForChecks(request)
-    }
-    fun isUuidToProfileApiRunning(): Boolean {
-        val request = Request.Builder().url("https://sessionserver.mojang.com/session/minecraft/profile/c6d2219bc8a54ccda8165328b2b32653/").build()
-        return executeRequestForChecks(request)
-    }
+    fun isUsernameToUuidApiRunning() = executeRequestForChecks(Request(url = "https://api.mojang.com/users/profiles/minecraft/SmushyTaco/".toHttpUrl()))
+    fun isUuidToProfileApiRunning() = executeRequestForChecks(Request(url = "https://sessionserver.mojang.com/session/minecraft/profile/c6d2219bc8a54ccda8165328b2b32653/".toHttpUrl()))
 }
